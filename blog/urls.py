5a51208e7796685277e8ad9d65de9cd23bf2f511
shahad aldaihani <ms.shoodii@gmail.com>
2020-12-05 18:02:14 +0300

    added static URL in setting and inURLS file

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
